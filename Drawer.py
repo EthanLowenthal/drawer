d89f0871192a7bd40ce5a136d30a917f7910e3ca
Ethan <elowenthal16@student.hausner.com>
2016-02-07 12:51:09 -0800

    functins

--- Drawer.py
+++ Drawer.py
@@ -10,19 +10,30 @@ x_pos = 60
 y_pos = 60
 color = colors["Red"]
 
+
 def draw_rect():
     pygame.draw.rect(screen, color, (x_pos,y_pos,size_x,size_y))
 
 def move_rect():
-    pressed = pygame.key.get_pressed
-    if(pressed()[pygame.K_d]):
-        x_pos += 3
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d or pygame.K_RIGHT:
+                x_pos += 60
+
+
+def check_for_quit():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and  event.key == pygame.K_ESCAPE or pygame.QUIT:
+                done = True
+                pygame.quit()
+
 
 def main():
     while not done:
         move_rect()
         draw_rect()
-        pygame.dispaly.update()
+        check_for_quit()
+        pygame.display.update()
 
 screen.fill(colors["White"])
 
