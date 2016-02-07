bd5ed454a69a198808b4662406b0aebf53710833
Ethan <elowenthal16@student.hausner.com>
2016-02-07 11:38:58 -0800

    functins

--- Drawer.py
+++ Drawer.py
@@ -1,14 +1,31 @@
 import pygame
 from Colors import colors
-pygame.init()
 screen = pygame.display.set_mode((800,600))
-done = False
+pygame.init()
 
+done = False
+size_x = 60
+size_y = 60
+x_pos = 60
+y_pos = 60
+color = colors["Red"]
+
+def draw_rect():
+    pygame.draw.rect(screen, color, (x_pos,y_pos,size_x,size_y))
+
+def move_rect():
+    pressed = pygame.key.get_pressed
+    if(pressed()[pygame.K_d]):
+        x_pos += 3
+
+def main():
+    while not done:
+        move_rect()
+        draw_rect()
+        pygame.dispaly.update()
 
 screen.fill(colors["White"])
 
 while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE or event.type == pygame.QUIT:
-            pygame.QUIT()
+    main()
     pygame.display.flip()
