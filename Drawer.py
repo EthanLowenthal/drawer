cc15ea19f8d9a1cc59fe3aefea20314d9d6362af
Ethan <elowenthal16@student.hausner.com>
2016-02-09 20:32:24 -0800

    Cleaning up functions.

--- Drawer.py
+++ Drawer.py
@@ -4,6 +4,10 @@ from Colors import colors
 screen = pygame.display.set_mode((800,600))
 pygame.init()
 
+clock = pygame.time.Clock()
+pygame.mouse.set_visible(0)
+
+
 done = False
 size_x = 60
 size_y = 60
@@ -14,26 +18,18 @@ color = colors["Red"]
 
 screen.fill(colors["White"])
 
-
+pygame.display.flip()
 
 def draw_rect():
+    print('draw_rect')
     pygame.draw.rect(screen, color, (x_pos,y_pos,size_x,size_y))
 
-def move_rect():
+
+def borders():
+    print('borders')
     global x_pos
     global y_pos
 
-    if(pygame.key.get_pressed()[pygame.K_w]):
-         y_pos += 3
-    if(pygame.key.get_pressed()[pygame.K_s]):
-         y_pos -= 3
-    if(pygame.key.get_pressed()[pygame.K_d]):
-         x_pos += 3
-    if(pygame.key.get_pressed()[pygame.K_a]):
-         x_pos -= 3
-    borders()
-
-def borders():
     if x_pos > 790:
          x_pos -= 3
     if x_pos < 10:
@@ -43,13 +39,43 @@ def borders():
     if y_pos < 10:
          y_pos += 3
 
-def main():
-    while not done:
-        draw_rect()
-        move_rect()
+def move_rect():
+    print('move_rect')
+    global x_pos
+    global y_pos
 
+    for event in pygame.event.get():
+        print('keyboard')
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+            x_pos -= 3
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+            x_pos += 3
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+            y_pos -= 3
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+            y_pos += 3
+        pygame.display.update()
+
+
+def check_for_quit():
+    print('check_for_quit')
+    global done
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.QUIT()
+
+
+def main():
+    print('main')
+    check_for_quit()
+    move_rect()
+    borders()
+    draw_rect()
 
 
 while not done:
+    print('loop')
     main()
-    pygame.display.flip()
+    pygame.display.update()
+    print('update')
